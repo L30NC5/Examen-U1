7d90e01fbe8bed1484734a5b27e20589ec7b1533
L30NC5 <20233tn050@ute.edu.mx>
2024-05-24 11:32:25 -0600

    V3

--- MARIA.java
+++ MARIA.java
@@ -1,6 +1,6 @@
 public class MARIA {
     public static void main(String[] args) {
-        System.out.println("HOLA MUNDO, MARIA!!");
+        System.out.println("HOLA MUNDO33, MARIA!!");
     }
 }
 
